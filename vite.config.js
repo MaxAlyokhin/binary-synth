52e250837ac4879f87fa895940a6d2d746368743
Max Alyokhin <55048182+MaxAlyokhin@users.noreply.github.com>
2023-11-04 22:45:51 +0300

    add https for external ip sharing

--- vite.config.js
+++ vite.config.js
@@ -3,10 +3,12 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import { viteSingleFile } from 'vite-plugin-singlefile'
+import mkcert from 'vite-plugin-mkcert'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [vue(), viteSingleFile({ removeViteModuleLoader: true })],
+    server: { https: true },
+    plugins: [vue(), viteSingleFile({ removeViteModuleLoader: true }), mkcert()],
     resolve: {
         alias: {
             '@': fileURLToPath(new URL('./src', import.meta.url)),
