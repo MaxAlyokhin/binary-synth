bf91bb08d1a465c4d41b7116cd289d73ffdefbfa
Max Alyokhin <55048182+MaxAlyokhin@users.noreply.github.com>
2024-05-10 17:06:41 +0300

    add scales to the Interactive input

--- src/components/ControlPanel/InteractiveInput.vue
+++ src/components/ControlPanel/InteractiveInput.vue
@@ -60,6 +60,7 @@ function mousemoveHandler(event) {
     )
 }
 
+// Через каждые 100 пикселей шаг мощности
 function getInputValueFactor(verticalDifference) {
     if (verticalDifference > 0) {
         if (verticalDifference > 300) return 0.001
@@ -103,18 +104,32 @@ watch(valid, (newValue) => {
             <div class="letter">{{ props.letter }}</div>
         </div>
 
-        <div v-show="isInteractiveMode" class="factor">x{{ inputValueFactor }}</div>
+        <div v-show="isInteractiveMode" class="scales">
+            <div class="factor">x{{ inputValueFactor }}</div>
+
+            <div class="gorizontal" :style="{ left: `${initialClientX}px`, borderWidth: `3px` }">{{ initialInputValue }}</div>
+
+            <div class="vertical" :style="{ top: `${initialClientY}px`, borderWidth: `3px` }">x1</div>
+            <div class="vertical" :style="{ top: `${initialClientY + 100}px` }">x0.1</div>
+            <div class="vertical" :style="{ top: `${initialClientY + 200}px` }">x0.01</div>
+            <div class="vertical" :style="{ top: `${initialClientY + 300}px` }">x0.001</div>
+            <div class="vertical" :style="{ top: `${initialClientY - 100}px` }">x10</div>
+            <div class="vertical" :style="{ top: `${initialClientY - 200}px` }">x100</div>
+            <div class="vertical" :style="{ top: `${initialClientY - 300}px` }">x1000</div>
+        </div>
     </div>
 </template>
 
 <style lang="scss" scoped>
+@import '@/assets/styles/vars.scss';
+
 .interactive-input {
     position: relative;
 
     .letter {
         position: absolute;
         color: #3e4656;
-        top: 4px;
+        top: 5px;
         right: 6px;
         z-index: 10;
         font-weight: 700;
@@ -140,6 +155,46 @@ input:focus {
     background: #090c0f;
 }
 
+.center {
+    position: fixed;
+    top: 0;
+    left: 0;
+    width: 10px;
+    height: 10px;
+    border-radius: 50%;
+    border: 1px solid $white;
+}
+
+.scales {
+    position: fixed;
+    top: 0;
+    left: 0;
+    width: 100vw;
+    height: 100vh;
+    z-index: 1;
+}
+.gorizontal,
+.vertical {
+    position: fixed;
+    top: 0;
+    left: 0;
+    z-index: 10;
+    padding: 5px;
+    color: $white;
+}
+
+.gorizontal {
+    height: 100vh;
+    width: 1px;
+    border-left: 1px solid $white;
+}
+
+.vertical {
+    width: 100vw;
+    height: 1px;
+    border-top: 1px solid $white;
+}
+
 .factor {
     position: fixed;
     top: 0;
@@ -151,6 +206,6 @@ input:focus {
     align-items: center;
     font-size: 80px;
     background: #000000a3;
-    z-index: 1;
+    color: $white;
 }
 </style>
